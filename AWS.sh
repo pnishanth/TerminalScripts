9c6655c138ac806f6cf12a8942d65f3a29ec5b0d
Nishanth <npannerselvam@zerionsoftware.com>
2018-01-10 11:35:50 -0500

    AWS

---
+++ AWS.sh
@@ -0,0 +1,10 @@
+##AWS
+
+$aws configure
+
+AWS Access Key ID [None]: xxxxxxxxxxxxxxx
+AWS Secret Access Key [None]: xxxxxxxxxxxxxx
+Default region name [None]: xxxxxxxxxxxx
+Default output format [None]: xxxxxxxxxxxxx
+
+aws s3 sync s3://"Reachable bucket URL to Sync" "Destination URL"
